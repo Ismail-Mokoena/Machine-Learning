0e95f6d397ea7b04484bd9c8eaf3378998a9fb8f
Ismail-Mokoena <ismailmokoena@icloud.com>
2024-03-12 12:05:52 -0500

    plot regresion

---
+++ Regression/Plot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+
+class Graph():
+    def __init__(self, 
+                 x:float, 
+                 y:float,
+                 pred:float, 
+                 c:str,
+                 c2:str,
+                 title: str, 
+                 x_label:str,
+                 y_label:str) -> None:
+        self.x = x
+        self.y = y
+        self.pred = pred
+        self.c = c
+        self.c2 = c2
+        self.title = title
+        self.x_label = x_label
+        self.y_label = y_label
+    
+    def show(self)->None:
+        plt.scatter(self.x,self.y,color=self.c)
+        # regresion line ~ predicted
+        plt.plot(self.x,self.pred,color=self.c2)
+        plt.title(self.title)
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
+        plt.show()  
+    
--- Regression/SimpleRegression.py
+++ Regression/SimpleRegression.py
@@ -1,8 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plot
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from Plot import Graph
 
 #import dataset
 dataset = pd.read_csv('Salary_Data.csv')
@@ -18,4 +18,15 @@ regress.fit(x_training,y_training)
 
 #predict test set results
 salary_prediction = regress.predict(x_test)
+salary_pred_training_set = regress.predict(x_training)
 print()
+#plot
+graph = Graph(x_training, 
+              y_training, 
+              salary_pred_training_set, 
+              "red", 
+              "blue",
+              "Salary vs Experience (Training Set)",
+              "Experience (Years)",
+              "Salary (Dollar)")
+graph.show()
